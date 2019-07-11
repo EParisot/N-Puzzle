fb61648e306c36179f1c385fb43563198043c4fa
eparisot <eparisot@student.42.fr>
2019-07-11 12:07:54 +0200

    solvability

--- map_builder.go
+++ map_builder.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
 func (env *Env) buildMap() {
@@ -17,18 +16,94 @@ func (env *Env) buildMap() {
 	}
 	newMap := make([]*cell, env.size*env.size)
 	env.grid.mapping = newMap
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	r := rand.New(rand.NewSource(42)) //time.Now().UnixNano()))
 	newID := r.Intn(env.size * env.size)
 	fmt.Println("Building map...")
-	for y := 0; y < env.size; y++ {
-		for x := 0; x < env.size; x++ {
-			for env.isPresent(newID) {
-				newID = r.Intn(env.size * env.size)
-			}
-			env.grid.mapping[newID] = &cell{
-				X: x,
-				Y: y,
+	for env.checkSolvability() == false {
+		for y := 0; y < env.size; y++ {
+			for x := 0; x < env.size; x++ {
+				for env.isPresent(newID) {
+					newID = r.Intn(env.size * env.size)
+				}
+				env.grid.mapping[newID] = &cell{
+					X: x,
+					Y: y,
+				}
 			}
 		}
 	}
+}
+
+func (env *Env) checkSolvability() bool {
+	if env.grid.mapping[0] == nil {
+		return false
+	}
+	var currList []int
+	var finishedList []int
+	env.buildFinished()
+	finishedMap := env.finishedMap
+	inversions := 0
+	for y := 0; y < env.size; y++ {
+		for x := 0; x < env.size; x++ {
+			currList = append(currList, idxByXY(env.grid, x, y))
+			finishedList = append(finishedList, idxByXY(finishedMap, x, y))
+		}
+	}
+	// iter on ids
+	for pivot := range currList {
+		if currList[pivot] != 0 {
+			// find pivot in result
+			k := idxByVAL(finishedList, currList[pivot])
+			// for each next id in curr
+			for i := range currList[pivot+1:] {
+				// check if next val in curr < pos pivot in res
+				j := idxByVAL(finishedList, currList[pivot+i])
+				if j < k {
+					inversions++
+				}
+			}
+		}
+	}
+	// if size is ODD
+	if env.size%2 != 0 {
+		//inversions must be EVEN
+		if inversions%2 != 0 {
+			return false
+		}
+	} else {
+		// if blank row is ODD
+		if env.grid.mapping[0].Y%2 != 0 {
+			// inversions shoul be EVEN
+			if inversions%2 != 0 {
+				return false
+			}
+			// if blank row is EVEN
+		} else {
+			// inversions shoulb be ODD
+			if inversions%2 == 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func idxByVAL(list []int, val int) int {
+	i := 0
+	for i = range list {
+		if list[i] == val {
+			break
+		}
+	}
+	return i
+}
+
+func idxByXY(grid *grid, x, y int) int {
+	i := 0
+	for i = range grid.mapping {
+		if grid.mapping[i].X == x && grid.mapping[i].Y == y {
+			break
+		}
+	}
+	return i
 }
